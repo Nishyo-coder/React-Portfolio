0c90945311073c8b776dccf424765f3bc6e9a92a
Nish Mitchell <shanishamit@gmail.com>
2021-07-28 11:15:35 -0400

    changing layout to pages

--- portfolio-01/src/App.js
+++ portfolio-01/src/App.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return (
-    <div className="App">
-     Hi
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
