785bcabd0257622aa4e2ca35b81165558de91883
Nish Mitchell <shanishamit@gmail.com>
2021-07-28 18:28:28 -0400

    adding images, form, and css

--- portfolio-01/src/components/pages/About.js
+++ portfolio-01/src/components/pages/About.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import'../../App.css'
 
 export default function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <img src="/images/nish.jpeg" alt=""/>
       <p>
       I'm a non-profit manager and web developer focused on technology advocacy to help develop platforms to assist kids from low socioeconomic areas.
       </p>
 
-      <img src="/images/nish.jpeg" alt=""/>
+      
     </div>
   );
 }
