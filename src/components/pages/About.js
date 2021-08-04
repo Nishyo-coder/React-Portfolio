65ae7752a1de9bc3a2c0f15399700fc2af4657ba
Nish Mitchell <shanishamit@gmail.com>
2021-08-04 09:48:54 -0400

    images

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ import'../../App.css'
 export default function About() {
   return (
     <div>
-      <img src="public/images/nish.jpeg" alt=""/>
+      <img src="/images/nish.JPEG" alt=""/>
       <h1>Hi, I'm Shanisha</h1>
           <h2> I'm a non-profit manager and web developer focused on technology advocacy to help develop platforms to assist kids from low socioeconomic areas.</h2>
 
